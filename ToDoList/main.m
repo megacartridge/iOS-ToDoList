ec08adb042e66104f908e869d1f5203cebc4196f
megacartridge <mdltrack@gmail.com>
2014-03-26 16:40:07 -0400

    Source Code
    Here it is.

---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Bennett Gaddes (2007 Macbook) on 3/26/14.
+//  Copyright (c) 2014 Bennett Gaddes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
